e6541045ab292d08952b1bb8e3a3490004157f6c
Gowsikakho <arulgowsi3357@gmail.com>
2026-01-19 11:41:43 +0530

    middleware issue

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,6 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher([
-  "/dashboard(.*)"
-]);
-
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req) && !auth().userId) {
-    return auth().redirectToSignIn();
-  }
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
